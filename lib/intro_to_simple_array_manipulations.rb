64b436b8f1c1b53d2701c57095bc86474071610f
Arina Victor <arinavictor@yahoo.com>
2019-08-11 21:22:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,4 +27,8 @@ def shift_with_args(array)
 end 
 
 def using_concat()
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  all_my_favs = my_favorite_things.concat(more_favs)
+end 
 
