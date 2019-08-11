6af1197917a410a09062bdc5385c853cca43de6a
Arina Victor <arinavictor@yahoo.com>
2019-08-11 20:55:43 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end 
+
+def using_unshift(array, string)
+   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+   new_neighborhood = "Staten Island"
+   bouroughs_in_nyc.unshift(new_neighborhood)
+end 
+
+def using_pop(array)
+  
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  deleted_string = using_pop(continents)
+  
+end
